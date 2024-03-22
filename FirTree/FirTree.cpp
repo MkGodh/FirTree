55251d668d0bba7f19bf89a444222247b78d0a33
MkGodh <fenryrvla@yandex.ru>
2024-03-22 17:26:23 +0300

    Добавьте файлы проекта.

---
+++ FirTree/FirTree.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    using namespace std;
+	int firTree;
+ string slash = "#";
+ string doubleSlash = "##";
+ cout << "Введите высоту ёлки: ";
+ cin >> firTree;
+ for (int row = 0; row != firTree;) {
+     for (int col = 0; col <= firTree; col++) {
+         if (col < firTree) {
+             cout << " ";
+ 
+         }
+         else if (col <= firTree) {
+             cout << slash << "\n";
+             firTree--;
+         }
+ 
+     }
+     slash = slash + doubleSlash;
+ }
+
+}
